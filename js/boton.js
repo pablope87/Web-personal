8726d8909baa4c930c08576f53ba9e6bf4a07008
pablo <pablopache87@gmail.com>
2023-09-23 17:53:42 -0300

    ultimos detalles

---
+++ js/boton.js
@@ -0,0 +1,6 @@
+function cambiarTexto() {
+  var elementoTexto = document.getElementById("texto1");
+  elementoTexto.innerHTML = "Tu trabajo va a llenar gran parte de tu vida, la única manera de estar realmente satisfecho es hacer lo que creas que es un gran trabajo y la única manera de hacerlo es amar lo que haces. Si no lo has encontrado aún, sigue buscando. Como con todo lo que tiene que ver con el corazón, lo sabrás cuando lo hayas encontrado.";
+}
+
+
